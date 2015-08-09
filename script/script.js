63743394d91e329195ddba0ecacdb392d3e7ed44
techpool <suryadeep.pal@icloud.com>
2015-08-09 18:45:22 +0530

    Firebase basic backend added

---
+++ script/script.js
@@ -0,0 +1,71 @@
+$(function(){
+      $(".header").typed({
+        strings: ["I N C U B A T I O N"],
+        typeSpeed: 40
+      });
+  });
+
+
+$(function(){
+        $(".element0").typed({
+            strings: ["^3000Learn new things<br>Innovate new ideas<br>Compete with yourself<br>Share with others"],
+            typeSpeed: 30, // typing speed
+            //backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+             // call function after typing is done
+        });
+    });
+
+/*
+setTimeout(function(){
+        $(".element1").css("display", "inherit");
+        $(".element1").typed({
+            strings: ["Innovate new ideas"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 5000);
+
+
+
+setTimeout(function(){
+        $(".element2").css("display", "inherit");
+        $(".element2").typed({
+            strings: ["Compete with yourself"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 7000);
+
+setTimeout(function(){
+        $(".element3").css("display", "inherit");
+        $(".element3").typed({
+            strings: ["Share with others"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 9000);
+
+setTimeout(function(){
+        $(".element4").css("display", "inherit");
+        $(".element4").typed({
+            strings: ["Join the club!"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 11000);*/
+
+
